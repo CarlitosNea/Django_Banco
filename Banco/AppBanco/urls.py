b26fe52bc6167bcab456c2e4952a17e85d75a5a6
CarlitosNea <cardarober@gmail.com>
2023-05-30 12:04:25 -0500

    metodos :)

--- Banco/AppBanco/urls.py
+++ Banco/AppBanco/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import *
+from AppBanco.views import *
 
 
-urlpatterns = [
+
+""" urlpatterns = [
     path('clientes',ListadoClientes.as_view(),name='Clientes'),
     path('usuarios',ListadoUsuario.as_view(),name='Usuarios'),
     path('lineas',ListadoLineas.as_view(),name='Lineas_credito'),
     path('creditos',ListadoCreditos.as_view(),name='Creditos')
-]
+] """
 
+
+urlpatterns = [
+    path('cliente',ListadoClientes.as_view(),name="clientes"),
+    path('insertar',InsertarCliente.as_view(),name="insertar"),
+    path('actualizar/<pk>',ActualizarCliente.as_view(),name="actualizar"),
+    path('eliminar/<pk>',Eliminar.as_view(),name="eliminar")
+]
--- Banco/AppBanco/views.py
+++ Banco/AppBanco/views.py
@@ -1,14 +1,19 @@
-from msilib.schema import ListView
-from re import template
-import tempfile
+from ast import Try
+from atexit import register
+from pydoc import cli
 from django.shortcuts import render
 from AppBanco.models import *
 from django.views.generic import *
+from django.http import *
+from django.utils.decorators import *
+from django.views.decorators.csrf import *
+import json
+
 
 # Create your views here.
 
 
-def listar (request):
+""" def listar (request):
     listarcli=Cliente.objects.all()
     return render (request,"index.html",{"lista_cli":listarcli})
 
@@ -30,5 +35,117 @@ class ListadoLineas(ListView):
 
 class ListadoCreditos(ListView):
     model=Creditos
-    template_name="creditos.html"
+    template_name="creditos.html" """
+
+
+
+class ListadoClientes(View):
+    def get(self,request):
+        datos:Cliente.objects.all().values()
+        datoscli=list(datos)
+        return JsonResponse(datoscli,safe=False)
+
+
+class InsertarCliente(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    def post(self,request):
+        datos=json.loads(request.body)
+        request.POST.GET('documento')
+        request.POST.GET('nombre')
+        request.POST.GET('apellido')
+        request.POST.GET('correo')
+        request.POST.GET('telefono')
+        request.POST.GET('direccion')
+        request.POST.GET('genero')
+
+        cli=Cliente.objects.create(documento=datos['documento'],nombre=datos['nombre'],apellido=datos['apellido'],correo=datos['correo'],telefono=datos['telefono'],direccion=datos['direccion'],genero=datos['genero'],)
+        cli.save()
+        return JsonResponse({'mensaje':'datos guardados'})
+
+
+class ActualizarCliente(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def put(self,request,pk):
+        try:
+            registro=Cliente.objects.get(pk=pk)
+        except Cliente.DoesNotExist:
+            return JsonResponse({"error":"el documento no exoste"})
+        data=json.loads(request.body)
+        registro.nombre=data.get('nombre')
+        registro.nombre=data.get('apellido')
+        registro.nombre=data.get('correo')
+        registro.nombre=data.get('telefono')
+        registro.nombre=data.get('direccion')
+        registro.nombre=data.get('genero')
+        registro.save()
+        return JsonResponse({'Mensaje':'datos actualizados'})
+
+
+
+class Eliminar(View):
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def delete(self,request,pk):
+        try:
+            registro=Cliente.objects.get(pk=pk)
+        except Cliente.DoesNotExist:
+            return JsonResponse({"error":"el documento no exoste"})
+        
+        registro.delete()
+        return JsonResponse({'mensaje':'dato eliminado'})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
